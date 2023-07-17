6db98617c705fabda0ed482eb8b7d6bd80db155b
Vinic1nn <viniciusafilho20@gmail.com>
2023-07-17 18:37:40 -0300

    some updates, API usage

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,22 @@ function isAnotherRed(anotherRed, redNow) {
     if (anotherRed)
         document.getElementById(redNow).style.backgroundColor = null;
 }
+window.onload = () => {
+    getAdvice()
+    chosenHero('hulkSection') 
+    turnRed('hulkSpan')
+}
 
-chosenHero('hulkSection') 
-turnRed('hulkSpan')
+
+const resDiv = document.querySelector('#results');
+
+function getAdvice() {
+    fetch(`https://api.adviceslip.com/advice`)
+        .then(response => response.json())
+        .then(adviceData => {
+            const adv = adviceData.slip;
+            resDiv.innerHTML = `<p>${adv.advice}</p>`;
+            console.log(adv.advice);
+        });
+}
+
